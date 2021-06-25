fce08da9d4963afef5ee6f98df7bd2ddb0c9f473
Joe Brown <jbrown@prometheum.com>
2021-06-25 05:13:13 -0400

    integrated react with next.js for client UI

--- server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
-
-app.use(express.static(path.join(process.cwd(), 'public')));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
